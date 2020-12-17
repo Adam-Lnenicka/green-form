8ed245fceb335f3d5e41827b324f02ea867626c3
Adam-Lnenicka <70516378+Adam-Lnenicka@users.noreply.github.com>
2020-12-18 00:13:27 +0100

    commit all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className ="signInBox">
+        
+      <img src="https://green.cdn.energy/branding/logo-r.svg" alt="green logo"></img>
+      <div className="form-header">
+        <h1>Operations studio</h1>
+        <p>Please enter your email below</p>
+      </div>
+
+      <form>
+        
+        <label htmlFor="email" className="email-label">Email Address</label>
+        <br/>
+        <div className="hi">
+        <input type ="text" id="email"></input>
+        </div>
+        
+        <div className="rememberMe">
+        <input type ="checkbox" id="rememberMe" ></input>
+        <label htmlFor="rememberMe" >Remember this device</label>
+        </div>
+
+      </form>
+      <button>Sign In</button>
+      </div>
     </div>
+
   );
 }
 
