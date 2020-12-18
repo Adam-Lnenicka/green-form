c7ebe660df6e6b47c9244c66ff58401a65081e32
Adam-Lnenicka <70516378+Adam-Lnenicka@users.noreply.github.com>
2020-12-18 11:26:07 +0100

    details

--- src/App.js
+++ src/App.js
@@ -10,21 +10,18 @@ function App() {
         <h1>Operations studio</h1>
         <p>Please enter your email below</p>
       </div>
-
-      <form>
-        
-        <div className="email">
-          <label htmlFor="email" >Email Address</label>
-          <input type ="text" id="email"></input>
-        </div>
-        
-        <div className="rememberMe">
-          <input type ="checkbox" id="rememberMe" ></input>
-          <label htmlFor="rememberMe" >Remember this device</label>
-        </div>
-
-      </form>
-      <button>Sign In</button>
+        <form>      
+          <div className="email">
+            <label htmlFor="email" >Email Address</label>
+            <input type ="text" id="email"></input>
+          </div>
+          
+          <div className="rememberMe">
+            <input type ="checkbox" id="rememberMe" ></input>
+            <label htmlFor="rememberMe" >Remember this device</label>
+          </div>
+        </form>        
+        <button>Sign In</button>
       </div>
     </div>
 
